382ed1b50732ea4f9970f474ba0d8ee8a18a068d
Brian Godoy <briangodoy01@outlook.com>
2023-12-23 11:53:27 -0300

    Version1.1

--- proyect_start.bat
+++
@@ -1,4 +0,0 @@
-start cmd.exe /k "cd F:\Apis\node-api-rest && npm run empezar"
-start cmd.exe /k "cd F:\CursoReact\blog_proyect && npm run dev"
-start C:\Users\User\AppData\Local\Postman\Postman.exe
-start C:\Users\User\AppData\Local\MongoDBCompass\MongoDBCompass.exe
